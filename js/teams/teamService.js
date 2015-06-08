785c13a3630e625a596414b8696dde11f9101d06
Mikel Blake <mikel.blake@gmail.com>
2015-06-08 17:05:23 -0600

    done through  step 6

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,44 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
+	this.addNewGame = function(gameObject){
+		var url = 'https://api.parse.com/1/classes/' + gameObject.homeTeam;
+		if(parseInt(gameObject.homeTeamScore) > parseInt(gameObject.opponentScore)){
+			gameObject.won = true;
+		} else {
+			gameObject.won = false;
+		}
+		return $http({
+			method: 'POST',
+			url: url,
+			data: gameObject
+		});
+	}
+
+	this.getTeamData = function(team){
+		var dfd = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http({
+			method: 'GET',
+			url: url
+		})
+		.then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(var i = 0; i < results.length; i++){
+				if(results[i].won === true){
+					wins++;
+				} else {
+					losses++;
+				}
+			}
+			results.wins = wins;
+			results.losses = losses;
+			dfd.resolve(results);
+		})
+
+		return dfd.promise;
+	}
 
 });
